234a34c162aad59b7f263b75a562c39205b8ea81
shigemiyagi <46915902+shigemiyagi@users.noreply.github.com>
2025-01-01 18:27:12 +0900

    Update script.js
    2枚選択後の表示時間を5秒に延長

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
             cardsChosenId.push(cardId);
             this.setAttribute('src', cardArray[cardId].img);
             if (cardsChosen.length === 2) {
-                setTimeout(checkForMatch, 500);
+                setTimeout(checkForMatch, 5000);
             }
         }
     }
