b2e6ac6e3c602c8be5d84b71b2fda9af4613f7af
shigemiyagi <46915902+shigemiyagi@users.noreply.github.com>
2025-01-01 18:31:05 +0900

    Update script.js
    クリアメッセージ修正

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener('DOMContentLoaded', () => {
         cardsChosenId = [];
 
         if (cardsWon.length === cardArray.length / 2) {
-            alert('Congratulations! You found them all!');
+            alert('全カードクリア！あなたはマツダマスターです！');
         }
     }
 
